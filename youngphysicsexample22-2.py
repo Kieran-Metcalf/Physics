7ccfe8bf5137cc329257d975cd1d46b7059c58ef
Kieran-Metcalf <31592937+Kieran-Metcalf@users.noreply.github.com>
2018-07-15 23:35:00 +0100

    Add files via upload

---
+++ youngphysicsexample22-2.py
@@ -0,0 +1,14 @@
+# This is a python2 file
+print "This is a python 2 file"
+print "Book Reference UNIVERSITY PHYSICS Young and Freedman example 22-2"
+print "Force between two point charges"
+print "Two point charges, q1=25nC and q2=-75nC, are seperated by a distance of 3.0cm. Find the magnitude and direction of a) the electric force that q1 exerts on q2; b)the electric force that q2 exerts on q1."
+q_one=25*10^-9
+permitivity = 
+print("permitivity is the degree of resistance a material has to to an electric field, the permitivity of free space is " + str(float(permit)) + "coulomb^2/N.m^2")
+print ("part a)")
+
+
+print ("the E field is equal in magnitude to the electrical potential gradient")
+
+print(E)
